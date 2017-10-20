a961618741040eb3b5da875e5c7e6a4f6679e7e0
Jack Bannister-Sutton <jackbannistersutton@gmail.com>
2017-10-20 15:13:15 +1300

    Initial Commit of program

---
+++ Micro.srcs/sources_1/new/MICRO_TOP.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10/20/2017 03:08:59 PM
+-- Design Name: 
+-- Module Name: MICRO_TOP - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity MICRO_TOP is
+    Port ( clk : in STD_LOGIC;
+           reset : in STD_LOGIC);
+end MICRO_TOP;
+
+architecture Behavioral of MICRO_TOP is
+
+begin
+
+
+end Behavioral;
